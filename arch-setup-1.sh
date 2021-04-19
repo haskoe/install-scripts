b27b3e5f193ee055363f11f034d09caf5f180ad4
heas <henrik@haskoe.dk>
2021-04-19 14:06:37 +0200

    tk

--- arch-setup-1.sh
+++ arch-setup-1.sh
@@ -1,7 +1,7 @@
 #git@github.com:haskoe/install-scripts.git
 # edit /etc/locale.gen manually
 
-sudo pacman -Sy --needed python-pip zsh-completions openssh inetutils keychain bash-completion mc cdrkit qemu fd zola ripgrep xorg-server xorg-apps lightdm-gtk-greeter i3-gaps firefox ranger i3status terminator base-devel thunar chromium xorg-xinit nodejs autorandr sshfs samba alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer imagemagick pavucontrol rxvt-unicode i3lock xautolock
+sudo pacman -Sy --needed python-pip zsh-completions openssh inetutils keychain bash-completion mc cdrkit qemu fd zola ripgrep xorg-server xorg-apps lightdm-gtk-greeter i3-gaps firefox ranger i3status terminator base-devel thunar chromium xorg-xinit nodejs autorandr sshfs samba alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer imagemagick pavucontrol rxvt-unicode i3lock xautolock tk
 
 sudo locale-gen
 sudo localectl set-locale LANG=en_DK.UTF-8
