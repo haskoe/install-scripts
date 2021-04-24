6b42bc4ebcbe699546893a73a23d2846367f5be5
haskoe <henrik@haskoe.dk>
2021-04-24 09:53:00 +0200

    vpn: env. variables needed in .bashrc

--- install-vpn.sh
+++ install-vpn.sh
@@ -1,2 +1,11 @@
 sudo pacman -S --needed openconnect freerdp
 pip3 install --user pykeepass
+
+# put these into ~/.bashrc ~/.zshrc
+# VPN_USER=
+# VPN_HUB=
+# KP_VPN_PATH=
+# KP_DB=
+
+# RD_USER=
+# RD_PC=
