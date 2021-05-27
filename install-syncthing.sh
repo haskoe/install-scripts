2a8c306293c422e9efbc5f8a827aa34ab5d8938f
haskoe <henrik@haskoe.dk>
2021-05-27 14:26:07 +0200

    syncthing issue

--- install-syncthing.sh
+++ install-syncthing.sh
@@ -1,3 +1,11 @@
 sudo pacman -S syncthing
 sudo systemctl enable syncthing@${USER}.service
 sudo systemctl start syncthing@${USER}.service
+
+# symlink as below according to: https://forum.syncthing.net/t/discovery-failures-connection-refused-for-v4-and-v6/15553/3
+sudo ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+# and restart service will perhaps get things working ....
+sudo systemctl restart systemd-networkd
+sudo systemctl restart systemd-resolved
+sudo systemctl restart syncthing@${USER}.service
+
